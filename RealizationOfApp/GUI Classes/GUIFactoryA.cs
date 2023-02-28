1e39a5cad2dcf71795a1186f4a92f585bffd8ad7
Виталий Ростока <rostoka.vit@yandex.ru>
2023-02-28 22:19:13 +0500

    delete mess

--- RealizationOfApp/GUI Classes/GUIFactoryA.cs
+++ RealizationOfApp/GUI Classes/GUIFactoryA.cs
@@ -8,14 +8,12 @@ namespace RealizationOfApp
         {
             List<EventDrawableGUI> drawableGUIs = new();
             Textbox textbox = new();
-            textbox.SetSizeRect(100, 65);
+            textbox.SetSizeRect(200, 65);
             textbox.SetOutlineColorRect(Color.Black);
             textbox.SetOutlineThicknessRect(2);
             textbox.SetFillColorRect(new Color(133, 253, 251));
             textbox.SetColorText(Color.Black);
             textbox.SetSizeCharacterText(16);
-
-
             textbox.SetString("Add");
             textbox.SetPos(100, 32.5f);
             drawableGUIs.Add(new ButtonAdd(textbox));
@@ -34,31 +32,6 @@ namespace RealizationOfApp
             textbox.SetPos(textbox.GetPosition().X + textbox.GetSizeRect().X, textbox.GetSizeRect().Y / 2);
             textbox.SetString("Oriented");
             drawableGUIs.Add(new ButtonOriented(textbox));
-
-            textbox.SetPos(textbox.GetPosition().X + textbox.GetSizeRect().X, textbox.GetSizeRect().Y / 2);
-            textbox.SetString("Test");
-            var testButton = new EvButton(textbox);
-            testButton.OnMouseMoved +=
-                (object? obj, ICollection<EventDrawableGUI> huis, MouseMoveEventArgs e) =>
-                {
-                    if (testButton.IsAlive && testButton.textbox.Contains(e.X,e.Y))
-                    {
-                        testButton.textbox.SetFillColorRect(Color.Green);
-                    }
-                    else
-                    {
-                        testButton.textbox.SetFillColorRect(testButton.BuffColor);
-                    }
-                };
-            drawableGUIs.Add(testButton);
-            testButton.OnMouseButtonPressed += (object? source, ICollection<EventDrawableGUI> elementsOfGUI, MouseButtonEventArgs e)=>
-            {
-                if (testButton.IsAlive && source is Application application && textbox.Contains(e.X, e.Y))
-                {
-                    application.messageToUser.SetString("Hello");
-                }
-            };
-            
             return drawableGUIs;
         }
 
